c49b12b905d825988739503c66c573005ac25052
frente50 <Alfrente50@gmail.com>
2021-10-20 10:12:36 -0500

    first commit

---
+++ src/test/java/com/tienda/online/Microservicioventasonline/MicroservicioVentasOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.online.Microservicioventasonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioVentasOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
